4a2ab3da508ebe877fc89036cc57578124141162
Giadagenovesi <giada.g@outlook.it>
2023-03-23 16:22:39 +0100

    Completed MILESTONE 0 started MILESTONE 1

--- js/script.js
+++ js/script.js
@@ -1,10 +1,42 @@
 // inserisco tutte le immagini in un array.
-const imagesArray = ["img/01.jpg", "img/02.jpg", "img/03.jpg", "img/04.jpg", "img/05.jpg"];
 
+const images = [
+    {
+        image: 'img/01.webp',
+        title: 'Marvel\'s Spiderman Miles Morale',
+        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+    }, {
+        image: 'img/02.webp',
+        title: 'Ratchet & Clank: Rift Apart',
+        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+    }, {
+        image: 'img/03.webp',
+        title: 'Fortnite',
+        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+    }, {
+        image: 'img/04.webp',
+        title: 'Stray',
+        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+    }, {
+        image: 'img/05.webp',
+        title: "Marvel's Avengers",
+        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+    }
+];
+
+
+const imagesArray = images.map((curImg) => curImg.image);
 console.log(imagesArray);
 
+const descriptionTitle = images.map((curTitle) => curTitle.title)
+console.log(descriptionTitle);
+
+const text = images.map((curText) => curText.text)
+console.log(text);
+
+
 const sliderContainer = document.querySelector(".slider-items");
-console.log(sliderContainer);
+// console.log(sliderContainer);
 
 for (let i = 0; i < imagesArray.length; i++) {
     const currentImage = imagesArray[i];
@@ -12,10 +44,14 @@ for (let i = 0; i < imagesArray.length; i++) {
     const sliderItem = `
         <div class="item">
             <img src="${currentImage}" alt="">
+        </div>
+        <div class="description">
+            <h2> </h2>
+            <p></p> 
         </div>`;
 
     console.log(sliderItem);
-sliderContainer.innerHTML += sliderItem;
+    sliderContainer.innerHTML += sliderItem;
 }
 
 // Tutte le immagini saranno nascoste, tranne la prima, che avrà una classe specifica che la renderà visibile.
@@ -32,26 +68,29 @@ items[activeItemIndex].classList.add("active");
 
 //bottone next
 const nextBtn = document.querySelector(".next");
-nextBtn.addEventListener ("click", function() {
-    
-    //rimuovo classe active dall'elemento precedente
-    items[activeItemIndex].classList.remove("active");
-    if (activeItemIndex < items.length - 1) {
-        // incremeto active index
-        activeItemIndex++;
-       
-    } else {
-        activeItemIndex = 0;
-    }
-
-    //Aggiungo classe active all'elemento successivo
-    items[activeItemIndex].classList.add("active");
-});
+nextBtn.addEventListener ("click", showNext);
 
 //bottone prev
 const prevBtn = document.querySelector(".prev");
-prevBtn.addEventListener ("click", function() {
-//rimuovo classe active dall'elemento precedente
+prevBtn.addEventListener ("click", showPrev);
+
+
+
+////////////////////////////////////////////////////////////////////////////
+// FUNCTION
+function showNext() {
+    items[activeItemIndex].classList.remove("active");
+    if (activeItemIndex < items.length - 1) {
+        // incremeto active index
+        activeItemIndex++;  
+    } else {
+        activeItemIndex = 0;
+    }
+    //Aggiungo classe active all'elemento successivo
+    items[activeItemIndex].classList.add("active");
+}
+function showPrev() {
+    //rimuovo classe active dall'elemento precedente
     items[activeItemIndex].classList.remove("active");
     if (activeItemIndex === 0 ) {
         activeItemIndex = items.length -1;       
@@ -59,9 +98,8 @@ prevBtn.addEventListener ("click", function() {
          // diminuisco active index
          activeItemIndex--;
     }
-
-     //Aggiungo classe active all'elemento successivo
-     items[activeItemIndex].classList.add("active"); 
-});
+    //Aggiungo classe active all'elemento successivo
+    items[activeItemIndex].classList.add("active"); 
+}
 
 
